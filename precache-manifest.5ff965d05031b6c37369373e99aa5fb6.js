62992b401b662ba63046f9ca5c3268aaac8eeedb
Sensei <karox1241@wp.pl>
2019-04-16 17:03:52 +0200

    Updates

---
+++ precache-manifest.5ff965d05031b6c37369373e99aa5fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "226fe2d4b7f9ff73f403",
+    "url": "/static/css/main.d62582ff.chunk.css"
+  },
+  {
+    "revision": "226fe2d4b7f9ff73f403",
+    "url": "/static/js/main.badeaba9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8d678e045b9e11083a9",
+    "url": "/static/css/2.31567dc9.chunk.css"
+  },
+  {
+    "revision": "f8d678e045b9e11083a9",
+    "url": "/static/js/2.118e7beb.chunk.js"
+  },
+  {
+    "revision": "1ab3c1a185dab5b3772e5275a3e1608e",
+    "url": "/index.html"
+  }
+];
